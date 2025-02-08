25c0998b4d569af35ce6ccd6c69fb90bb742ba65
LSNMCLNTL <elsonmacalintal@gmail.com>
2025-02-08 22:13:39 +0800

    added sql files

---
+++ project sql files/top_paying_jobs.sql
@@ -0,0 +1,24 @@
+/*
+Which companies offer the highest-paying remote Data Analyst jobs?
+This query helps identify companies that pay the most for remote Data Analyst roles.
+*/
+SELECT
+    job_title,
+    get_company_name.company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date::DATE
+FROM job_postings_fact as jp
+LEFT JOIN( 
+    SELECT 
+    company_id,
+    name as company_name
+    FROM company_dim 
+) get_company_name on jp.company_id = get_company_name.company_id
+WHERE 
+    salary_year_avg IS NOT NULL 
+    AND job_work_from_home = TRUE 
+    AND job_title_short = 'Data Analyst'
+ORDER BY salary_year_avg DESC
+LIMIT 20
---
+++ project sql files/top_paying_skills.sql
@@ -0,0 +1,43 @@
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        get_company_name.company_name,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date::DATE
+    FROM job_postings_fact as jp
+    LEFT JOIN( 
+        SELECT 
+        company_id,
+        name as company_name
+        FROM company_dim 
+    ) get_company_name on jp.company_id = get_company_name.company_id
+    WHERE 
+        salary_year_avg IS NOT NULL 
+        AND job_work_from_home = TRUE 
+        AND job_title_short = 'Data Analyst'
+    ORDER BY salary_year_avg DESC
+    LIMIT 10
+)
+SELECT
+skills as skill_name,
+COUNT(skjob.job_id) as skill_count
+FROM top_paying_jobs toppay
+INNER JOIN skills_job_dim as skjob on toppay.job_id=skjob.job_id
+INNER JOIN skills_dim as skim on skjob.skill_id = skim.skill_id
+GROUP BY skill_name
+ORDER BY skill_count DESC
+/*
+Query for counting the occurrences of each skill 
+SELECT
+skills as skill_name,
+COUNT(skjob.job_id) as skill_count
+FROM top_paying_jobs toppay
+INNER JOIN skills_job_dim as skjob on toppay.job_id=skjob.job_id
+INNER JOIN skills_dim as skim on skjob.skill_id = skim.skill_id
+GROUP BY skill_name
+ORDER BY skill_count DESC
+*/
